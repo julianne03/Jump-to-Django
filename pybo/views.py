62ffa04c7a1d4193245bfbce1ee2a80349eeefc1
s2019w33@e-mirim.hs.kr <s2019w33@e-mirim.hs.kr>
2020-12-07 18:43:12 +0900

    select model and template (#4)

--- pybo/views.py
+++ pybo/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from django.http import HttpResponse
+from .models import Question
 
 def index(request) :
-    return HttpResponse("안녕하세요 pybo에 오신 것을 환영합니다!")
+    # 조회 결과를 작성일시 역순으로 정렬
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    #render 함수 - 조회된 질문목록 데이터를 지정된 파일에 적용하여 HTML로 변환해 주는 함수
+    #question_list.html - 템플릿 (장고에서 사용하는 태그들을 사용가능한 HTML 파일
+    return render(request, 'pybo/question_list.html', context)
 
-# Create your views here.
+def detail(request, question_id) :
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question':question}
+    return render(request, 'pybo/question_detail.html', context)

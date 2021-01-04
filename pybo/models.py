dbdabceceb4306bd3902b7d8a41693366e8f7637
s2019w33@e-mirim.hs.kr <s2019w33@e-mirim.hs.kr>
2021-01-04 21:50:54 +0900

    model change <- <author> add(#17)

--- pybo/models.py
+++ pybo/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Question(models.Model) :
     subject = models.CharField(max_length=200)  #제목
     content = models.TextField()
     create_date = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.subject
@@ -14,3 +15,4 @@ class Answer(models.Model) :
     question = models.ForeignKey(Question, on_delete=models.CASCADE)  #질문과 같이 삭제된다.
     content = models.TextField()
     create_date = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
